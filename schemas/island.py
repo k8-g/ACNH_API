43c5b82270699922a5d8cbfbfba161dae9d2a5f6
Kate Gerber <kate@gerber.id.au>
2024-07-12 07:18:23 +1000

    Create Island Schema

---
+++ schemas/island.py
@@ -0,0 +1,26 @@
+from init import db, ma
+
+
+class IslandSchema(ma.Schema):
+    class Meta:
+        fields = ("island_id", "name_of_island", "owner_of_island", "password", "is_admin")
+
+# to handle a single island object
+island_schema = IslandSchema(exclude=["password"])
+
+# to handle a list of island objects
+islands_schema = IslandSchema(many=True, exclude=["password"])
+
+
+# What Simon used in class:
+# class UserSchema(ma.Schema):
+#     class Meta:
+#         fields = ("island_id", "name_of_island", "owner_of_island", "password", "is_admin")
+
+# # to handle a single user object
+# user_schema = UserSchema(exclude=["password"])
+
+# # to handle a list of user objects
+# users_schema = UserSchema(many=True, exclude=["password"])
+
+
