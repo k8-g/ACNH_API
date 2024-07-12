c4c36266680f48c063a99ecb7547b2282ec22f7e
Kate Gerber <kate@gerber.id.au>
2024-07-12 11:15:35 +1000

    Updated table name in island.py

--- models/island.py
+++ models/island.py
@@ -1,13 +1,11 @@
 from init import db, ma
 
-class Island(db.Model):
+class IslandUser(db.Model):
     # name of the table
-    __tablename__ = "Island"
+    __tablename__ = "Islands"
 
     island_id = db.Column(db.Integer, primary_key=True)
     name_of_island = db.Column(db.String, nullable=False)
     owner_of_island = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-
-
