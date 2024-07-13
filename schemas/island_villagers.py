b5984c74f3695ebbb1fcf0a3ab6ab777aa6d9854
Kate Gerber <kate@gerber.id.au>
2024-07-14 03:38:17 +1000

    Updated to include nested fields

--- schemas/island_villagers.py
+++ schemas/island_villagers.py
@@ -1,8 +1,13 @@
 from init import ma
+from marshmallow import fields
 
 class IslandVillagersSchema(ma.Schema):
     class Meta:
         fields = ("island_villagers_id", "island_id", "villager_id", "comment_id")
+
+    island = fields.Nested('IslandSchema', exclude=('island_villagers', 'wanted'))
+    villager = fields.Nested('VillagerSchema')
+    comments = fields.Nested('CommentsSchema', many=True)
 
 # to handle a single island_villager object
 island_villager_schema = IslandVillagersSchema()
