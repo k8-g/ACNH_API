04a97b7cbf85c8cf414b598e44f18a496ef4f929
Kate Gerber <kate@gerber.id.au>
2024-07-14 07:07:53 +1000

    Added notes_controller.py

---
+++ controllers/notes_controller.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, request
+from init import db
+from models.notes import Notes
+from schemas.notes import notes_schema, note_schema
+from flask_jwt_extended import jwt_required
+
+# Create a blueprint
+notes_bp = Blueprint('notes', __name__)
+
+@notes_bp.route('/notes', methods=['GET'])
+def get_notes():
+    notes = Notes.query.all()
+    return notes_schema.dump(notes), 200
+
+@notes_bp.route('/notes/<int:id>', methods=['GET'])
+def get_note(id):
+    note = Notes.query.get_or_404(id)
+    return note_schema.dump(note), 200
+
+@notes_bp.route('/notes', methods=['POST'])
+@jwt_required()
+def add_note():
+    data = request.get_json()
+    new_note = Notes(
+        wanted_id=data['wanted_id'],
+        villager_id=data['villager_id'],
+        notes=data['notes']
+    )
+    db.session.add(new_note)
+    db.session.commit()
+    return note_schema.dump(new_note), 201
+
+@notes_bp.route('/notes/<int:id>', methods=['PUT'])
+@jwt_required()
+def update_note(id):
+    note = Notes.query.get_or_404(id)
+    data = request.get_json()
+    note.wanted_id = data['wanted_id']
+    note.villager_id = data['villager_id']
+    note.notes = data['notes']
+    db.session.commit()
+    return note_schema.dump(note), 200
+
+@notes_bp.route('/notes/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_note(id):
+    note = Notes.query.get_or_404(id)
+    db.session.delete(note)
+    db.session.commit()
+    return {"message": "Note deleted"}, 200
