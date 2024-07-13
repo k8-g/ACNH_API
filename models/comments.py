51cdbeb0384923b701e9d8db311b9e4bb4a67d7f
Kate Gerber <kate@gerber.id.au>
2024-07-14 09:07:55 +1000

    Edited to include explicit foreign keys

--- models/comments.py
+++ models/comments.py
@@ -4,8 +4,8 @@ class Comments(db.Model):
     __tablename__ = "comments"
 
     comments_id = db.Column(db.Integer, primary_key=True)
-    island_villagers_id = db.Column(db.Integer, db.ForeignKey('island_villagers.island_villagers_id'))
-    wanted_id = db.Column(db.Integer, db.ForeignKey('villagers_wanted.wanted_id'))
+    island_villagers_id = db.Column(db.Integer, db.ForeignKey('island_villagers.island_villagers_id', name='fk_comments_island_villagers'))
+    wanted_id = db.Column(db.Integer, db.ForeignKey('villagers_wanted.wanted_id', name='fk_comments_wanted'))
     island_comments = db.Column(db.Text, nullable=False)
 
     # Relationships
