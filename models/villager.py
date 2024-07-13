faa2ec30cd948e1799a85db05a59378f53712ef9
Kate Gerber <kate@gerber.id.au>
2024-07-14 03:17:33 +1000

    Updated villagers schema to included nested fields & updated model to include relationships

--- models/villager.py
+++ models/villager.py
@@ -1,4 +1,4 @@
-from init import db, ma
+from init import db
 
 class Villager(db.Model):
     __tablename__ = "villager"
@@ -10,3 +10,8 @@ class Villager(db.Model):
     birthday = db.Column(db.String, nullable=False)
     catchphrase = db.Column(db.String, nullable=False)
     hobbies = db.Column(db.String, nullable=False)
+
+    # Relationships
+    island_villagers = db.relationship('IslandVillagers', back_populates='villager')
+    wanted = db.relationship('VillagersWanted', back_populates='villager')
+    notes = db.relationship('Notes', back_populates='villager')
--- schemas/villager.py
+++ schemas/villager.py
@@ -1,8 +1,13 @@
 from init import ma
+from marshmallow import fields
 
 class VillagerSchema(ma.Schema):
     class Meta:
         fields = ("villager_id", "name", "species", "personality", "birthday", "catchphrase", "hobbies")
+
+    island_villagers = fields.Nested('IslandVillagersSchema', many=True, exclude=('villager',))
+    wanted = fields.Nested('VillagersWantedSchema', many=True, exclude=('villager',))
+    notes = fields.Nested('NotesSchema', many=True)
 
 # to handle a single villager object
 villager_schema = VillagerSchema()
