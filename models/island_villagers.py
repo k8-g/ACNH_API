f76b679f0bad00e100d525092e16b4de9be8eca0
Kate Gerber <kate@gerber.id.au>
2024-07-14 09:07:28 +1000

    Edited to include explicit foreign keys

--- models/island_villagers.py
+++ models/island_villagers.py
@@ -4,9 +4,9 @@ class IslandVillagers(db.Model):
     __tablename__ = "island_villagers"
 
     island_villagers_id = db.Column(db.Integer, primary_key=True)
-    island_id = db.Column(db.Integer, db.ForeignKey('islands.island_id'), nullable=False)
-    villager_id = db.Column(db.Integer, db.ForeignKey('villagers.villager_id'), nullable=False)
-    comment_id = db.Column(db.Integer, db.ForeignKey('comments.comments_id'))
+    island_id = db.Column(db.Integer, db.ForeignKey('islands.island_id', name='fk_island_villagers_island'), nullable=False)
+    villager_id = db.Column(db.Integer, db.ForeignKey('villagers.villager_id', name='fk_island_villagers_villager'), nullable=False)
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.comments_id', name='fk_island_villagers_comment'))
 
     # Relationships
     island = db.relationship('IslandUser', back_populates='island_villagers')
