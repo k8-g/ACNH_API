3487da75a44d3867dd5aa965da0efe641412c1d4
Kate Gerber <kate@gerber.id.au>
2024-07-16 00:16:21 +1000

    Edited auth_controller to include integrity error codes & messages

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,32 +1,56 @@
 from flask import Blueprint, request
+from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
 from init import db, bcrypt, jwt
 from models.island import Island
-from schemas.island import island_schema
+from schemas.island import island_schema, IslandSchema
 from flask_jwt_extended import create_access_token
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint("auth", __name__,)
 
 @auth_bp.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    owner_of_island = data.get('owner_of_island')
-    password = data.get('password')
-    name_of_island = data.get('name_of_island')
-    is_admin = data.get('is_admin', False)
+def register_user():
+    try:
+        # get the data from the body of the request
+        body_data = IslandSchema().load(request.get_json())
 
-    if Island.query.filter_by(owner_of_island=owner_of_island).first():
-        return {"message": "User already exists"}, 400
+        # create an instance of the Island model
+        user = Island(
+            name_of_island=body_data.get("name_of_island"),
+            owner_of_island=body_data.get("owner_of_island")
+            )
+        # extract the password from the body
+        password = body_data.get("password")
 
-    new_user = Island(
-        name_of_island=name_of_island,
-        owner_of_island=owner_of_island,
-        password=bcrypt.generate_password_hash(password).decode('utf-8'),
-        is_admin=is_admin
-    )
-    db.session.add(new_user)
-    db.session.commit()
+        # hash the password
+        if password:
+            user.password = bcrypt.generate_password_hash(password).decode("utf-8")
 
-    return island_schema.dump(new_user), 201
+
+        # data = request.get_json()
+        # owner_of_island = data.get('owner_of_island')
+        # password = data.get('password')
+        # name_of_island = data.get('name_of_island')
+        # is_admin = data.get('is_admin', False)
+
+        # if Island.query.filter_by(owner_of_island=owner_of_island).first():
+        #     return {"message": "User already exists"}, 400
+
+        # user = Island(
+        #     name_of_island=name_of_island,
+        #     owner_of_island=owner_of_island,
+        #     password=bcrypt.generate_password_hash(password).decode('utf-8'),
+        #     is_admin=is_admin
+        # )
+        db.session.add(user)
+        db.session.commit()
+
+        return island_schema.dump(user), 201
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            return {"error": f"Missing info; {err.orig.diag.column_name} is required"}, 409
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {"error": "Owner name already in use"}, 409
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
