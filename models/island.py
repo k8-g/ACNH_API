96d9a37530b8f508508aeade71a17ae04947ee94
Kate Gerber <kate@gerber.id.au>
2024-07-15 22:43:44 +1000

    Added comments

--- models/island.py
+++ models/island.py
@@ -1,14 +1,17 @@
+# Instead of user, Island
 from init import db, bcrypt
 
 class Island(db.Model):
     # name of the table
     __tablename__ = "Island"
 
+    # attributes of the table
     island_id = db.Column(db.Integer, primary_key=True)
     name_of_island = db.Column(db.String, nullable=False)
     owner_of_island = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+
 
     # Back reference to IslandVillagers and VillagersWanted
     # island_villagers = db.relationship('IslandVillagers', back_populates='island')
