8736bc6e9ac1e9fc98d3d26d503390ee4d1293ec
Kate Gerber <kate@gerber.id.au>
2024-07-18 00:21:57 +1000

    Added villager info for seeding & testing

--- src/controllers/cli_controller.py
+++ src/controllers/cli_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from init import db, bcrypt
 
 from models.island import Island
+from models.villager import Villager
 
 db_commands = Blueprint("db", __name__)
 
@@ -26,16 +27,31 @@ def seed_tables():
             password=bcrypt.generate_password_hash("123456").decode("utf-8"),
             is_admin=True
         ),
-        Island(
-            name_of_island="Second Island",
-            owner_of_island="Katherine",
-            password=bcrypt.generate_password_hash("123456").decode("utf-8"),
-            is_admin=False
-        ),
+        # Island(
+        #     name_of_island="Second Island",
+        #     owner_of_island="Katherine",
+        #     password=bcrypt.generate_password_hash("123456").decode("utf-8"),
+        #     is_admin=False
+        # ),
         # Add more Islands as needed
     ]
-    
+
+    villagers = [
+        Villager(
+            villager_id="430",
+            name="Judy",
+            gender="Female",
+            species="Bear cub",
+            personality="Snooty",
+            birthday="March 10",
+            catchphrase="myohmy",
+            hobbies="music"
+        )
+    ]
+
+
     # Add islands to the database session
     db.session.add_all(islands)
+    db.session.add_all(villagers)
     db.session.commit()
     print("Tables seeded")
