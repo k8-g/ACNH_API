96f6cf543a99fc221c2c6923646b119971d800b0
Kate Gerber <kate@gerber.id.au>
2024-07-21 03:49:47 +1000

    Updated Island model, schema & controller

--- src/models/island.py
+++ src/models/island.py
@@ -5,15 +5,22 @@ class Island(db.Model):
     __tablename__ = "island"
 
     # attributes of the table
-    island_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     island_name = db.Column(db.String, nullable=False)
-
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
-    user = db.relationship('User', back_populates='islands')
-    villagers = db.relationship('Villager', back_populates='island')
-    comments = db.relationship('Comment', back_populates='island')
+    # Relationships
+    # user = db.relationship('User', back_populates='islands')
+    user = db.relationship('User', backref='islands')
+    
+    # island_villagers = db.relationship('IslandVillager', back_populates='island')
+    # wanted_villagers = db.relationship('WantedVillager', back_populates='island')
+
+    # user = db.relationship('User')
+    # user = db.relationship('User', back_populates='islands')
+    # villagers = db.relationship('Villager', back_populates='island')
+    # comments = db.relationship('Comment', back_populates='island')
     # cascade part = if an island is deleted, the comments get deleted too
     # comments = db.relationship('Comment', back_populates='island', cascade="all, delete")
     # notes = db.relationship('Note', back_populates="islands")
--- src/schemas/island.py
+++ src/schemas/island.py
@@ -3,17 +3,17 @@ from marshmallow import fields
 
 class IslandSchema(ma.Schema):
 
-    # notes = fields.List(fields.Nested('NoteSchema', exclude=["owner"]))
     user = fields.Nested('UserSchema', only=["name","id"])
-    comments = fields.List(fields.Nested('CommentSchema', exclude=["island"]))
+    island_villagers = fields.Nested('IslandVillagerSchema', many=True, exclude=['island'])
+    wanted_villagers = fields.Nested('WantedVillagerSchema', many=True, exclude=['island'])
 
-    # user and comments below link to above
     class Meta:
-        fields = ("island_id", "island_name", "user", "comments")
+        fields = ("id", "island_name", "user")
+        # fields = ("id", "island_name", "user", "island_villagers", "wanted_villagers")
 
-
-# to handle a single island object
 island_schema = IslandSchema()
-
-# to handle a list of island objects
 islands_schema = IslandSchema(many=True)
+
+    # notes = fields.List(fields.Nested('NoteSchema', exclude=["owner"]))
+    # comments = fields.List(fields.Nested('CommentSchema', exclude=["island"]))
+
