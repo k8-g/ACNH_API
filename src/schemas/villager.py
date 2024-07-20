9c8093d63d5c3cbbc1c365a1b55189e919303a18
Kate Gerber <kate@gerber.id.au>
2024-07-21 03:51:48 +1000

    Updated Villager model, schema & controller

--- src/schemas/villager.py
+++ src/schemas/villager.py
@@ -2,18 +2,18 @@ from init import ma
 from marshmallow import fields
 
 class VillagerSchema(ma.Schema):
-
-    user = fields.Nested('UserSchema', only=["name"])
+    island_villagers = fields.Nested('IslandVillagerSchema', many=True, exclude=['villager'])
+    wanted_villagers = fields.Nested('WantedVillagerSchema', many=True, exclude=['villager'])
 
     class Meta:
-        fields = ("villager_id", "name", "gender", "species", "personality", "birthday", "catchphrase", "hobbies", "user")
+        fields = ("id", "name", "gender", "species", "personality", "birthday", "catchphrase", "hobbies")
+        # fields = ("id", "name", "gender", "species", "personality", "birthday", "catchphrase", "hobbies", "island_villagers", "wanted_villagers")
+
+villager_schema = VillagerSchema()
+villagers_schema = VillagerSchema(many=True)
+
+    # user = fields.Nested('UserSchema', only=["name"])
 
     # island_villagers = fields.List(fields.Nested('IslandVillagersSchema', exclude=('villager',)))
     # wanted = fields.List(fields.Nested('VillagersWantedSchema', exclude=('villager',)))
     # notes = fields.List(fields.Nested('NotesSchema'))
-
-# to handle a single villager object
-villager_schema = VillagerSchema()
-
-# to handle a list of villager objects
-villagers_schema = VillagerSchema(many=True)
