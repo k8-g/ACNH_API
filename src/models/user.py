3405eec873dab247efdc739d187ceff11b027220
Kate Gerber <kate@gerber.id.au>
2024-07-23 00:56:19 +1000

    Added Flask Validation to User & island

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,6 @@
 from init import db, ma
 from marshmallow import fields
-# from marshmallow.validate import Regexp
+from marshmallow.validate import Regexp
 
 
 class User(db.Model):
@@ -18,8 +18,13 @@ class User(db.Model):
 class UserSchema(ma.Schema):
     islands = fields.List(fields.Nested('IslandSchema', exclude=["user"]))
 
+    email = fields.String(required=True, validate=Regexp("^\S+@\S+\.\S+$", error="Invalid Email Format"))
+
+    password = fields.String(required=True, validate=Regexp("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", error="Minimum six characters, at least one letter and one number"))
+
     class Meta:
         fields = ("id", "name", "email", "password", "is_admin", "islands")
+        ordered = True
 
 
 user_schema = UserSchema(exclude=["password"])
